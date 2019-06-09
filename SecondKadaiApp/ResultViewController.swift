d5c26d471d7801cd96d72554bb24f2face624362
なまえ <>
2019-06-09 17:34:57 +0900

    Initial Commit

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 伊藤實梨 on 2019/06/09.
+//  Copyright © 2019 minori.ito. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 伊藤實梨 on 2019/06/09.
+//  Copyright © 2019 minori.ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
+}
+
