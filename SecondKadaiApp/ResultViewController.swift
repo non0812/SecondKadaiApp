238a56530a8f93fe208e446090f08e90f34cfb6c
なまえ <>
2019-06-14 17:37:31 +0900

    SecondkadaiApp

--- SecondKadaiApp/ResultViewController.swift
+++ SecondKadaiApp/ResultViewController.swift
@@ -1,31 +1,16 @@
-//
-//  ResultViewController.swift
-//  SecondKadaiApp
-//
-//  Created by 伊藤實梨 on 2019/06/09.
-//  Copyright © 2019 minori.ito. All rights reserved.
-//
-
 import UIKit
 
 class ResultViewController: UIViewController {
-
-    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var sayHello: UILabel!
+    
+    var x : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        sayHello.text = "こんにちは、\(x!)さん"
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -1,21 +1,22 @@
-//
-//  ViewController.swift
-//  SecondKadaiApp
-//
-//  Created by 伊藤實梨 on 2019/06/09.
-//  Copyright © 2019 minori.ito. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var namae: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // no need to add
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        // ResultViewControllerのxにnamaeを代入
+        resultViewController.x = namae.text
+    }
+    
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
     }
+    
 }
-
